4c1381461bac7245e9d2c5eff58b9e7618763a92
Pradyun11 <technomyth16@gmail.com>
2020-08-23 12:57:25 +0530

    done

--- sketch.js
+++ sketch.js
@@ -1,47 +1,12 @@
-var ball,database,ballPositionRef,position;
+var canvas,database,gameState,playerCount,form,player,game;
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    canvas = createCanvas(400,400);
+
     database = firebase.database();
-    ballPositionRef =  database.ref("ball/position");
-    ballPositionRef.on("value",readPosition,showError);
-}
 
-function draw(){
-    background("white");
-    if(position !== undefined){
-        if(keyDown(LEFT_ARROW)){
-            changePosition(-1,0);
-        }
-        else if(keyDown(RIGHT_ARROW)){
-            changePosition(1,0);
-        }
-        else if(keyDown(UP_ARROW)){
-            changePosition(0,-1);
-        }
-        else if(keyDown(DOWN_ARROW)){
-            changePosition(0,+1);
-        }
-    }
-    drawSprites();
-}
-
-function changePosition(x,y){
-    database.ref("ball/position").set({
-        'x': position.x + x,
-        'y': position.y + y
-    })
-    
-}
-
-function readPosition(data){
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
-}
-
-function showError(){
-    console.log("error");
+    gameState = 0;
+    game = new Game();
+    game.getState();
+    game.start();
 }
